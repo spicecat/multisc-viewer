c16a54fee214900653afe261c26bf85c0abe0c07
Jason Xu <Xinwen31415@gmail.com>
2025-03-03 13:19:27 -0600

    fixed color file resolution

--- src/utils/Daemon.ts
+++ src/utils/Daemon.ts
@@ -1,125 +1,109 @@
-import type { ChildProcessWithoutNullStreams } from "node:child_process";
-import { spawn } from "node:child_process";
-import { randomBytes } from "node:crypto";
+import type { ChildProcessWithoutNullStreams } from 'node:child_process';
+import { spawn } from 'node:child_process';
+import { randomBytes } from 'node:crypto';
 
 interface DSRecord {
-  ds: string;
-  loaders: Set<string>;
-}
-
-interface ProcRecord {
-  proc: ChildProcessWithoutNullStreams;
-  acks: ((opid: string) => boolean)[];
-  datasets: DSRecord[];
-  idle: Promise<void>;
+	ds: string;
+	loaders: Set<string>;
 }
 
 export class Daemon {
-  private readonly proc: ChildProcessWithoutNullStreams;
-  private _acks: ((opid: string) => boolean)[];
-  private _datasets: DSRecord[];
-  private _idle: Promise<void>;
+	private readonly proc: ChildProcessWithoutNullStreams;
+	private _acks: ((opid: string) => boolean)[];
+	private _datasets: DSRecord[];
+	private _idle: Promise<void>;
 
-  public constructor() {
-    this.proc = spawn("Rscript", ["daemon.r"]);
+	public constructor() {
+		this.proc = spawn('Rscript', ['daemon.r']);
 
-    this.proc.stdin.on("error", (error: NodeJS.ErrnoException) => {
-      if (error.code === "EPIPE")
-        console.error("EPIPE error: child process pipe is closed.", error);
-      else console.error("Error on stdin:", error);
-    });
+		this.proc.stdin.on('error', (error: NodeJS.ErrnoException) => {
+			if (error.code === 'EPIPE') console.error('EPIPE error: child process pipe is closed.', error);
+			else console.error('Error on stdin:', error);
+		});
 
-    this._acks = [];
-    this._datasets = [];
-    this._idle = Promise.resolve();
+		this._acks = [];
+		this._datasets = [];
+		this._idle = Promise.resolve();
 
-    let data = "";
-    this.proc.stdout.on("data", (chunk) => {
-      data += chunk;
+		let data = '';
+		this.proc.stdout.on('data', (chunk) => {
+			data += chunk;
 
-      const match = /^ack ([^\n]+)\n/.exec(data);
-      if (match !== null) {
-        const opid = match[1];
-        this._acks = this._acks.filter((ack) => ack(opid));
-        data = data.slice(match[0].length);
-      }
-    });
-  }
+			const match = /^ack ([^\n]+)\n/.exec(data);
+			if (match !== null) {
+				const opid = match[1];
+				this._acks = this._acks.filter((ack) => ack(opid));
+				data = data.slice(match[0].length);
+			}
+		});
 
-  public get idle(): Promise<void> {
-    return this._idle;
-  }
+		this.proc.stderr.on('data', (chunk) => console.error(chunk.toString()));
+	}
 
-  public get datasets(): DSRecord[] {
-    return this._datasets;
-  }
+	public get idle(): Promise<void> {
+		return this._idle;
+	}
 
-  public exec(
-    executor: (proc: ChildProcessWithoutNullStreams, opid: string) => void,
-  ): Promise<void> {
-    const opid = randomBytes(16).toString("hex");
+	public get datasets(): DSRecord[] {
+		return this._datasets;
+	}
 
-    const promise = new Promise<void>((resolve, reject) => {
-      this._idle.then(() => {
-        executor(this.proc, opid);
+	public exec(executor: (proc: ChildProcessWithoutNullStreams, opid: string) => void): Promise<void> {
+		const opid = randomBytes(16).toString('hex');
 
-        this._acks.push((id) => {
-          if (id === opid) {
-            resolve();
-            return true;
-          } else return false;
-        });
-      });
-    });
+		const promise = new Promise<void>((resolve, reject) => {
+			this._idle.then(() => {
+				executor(this.proc, opid);
 
-    this._idle = promise;
+				this._acks.push((id) => {
+					if (id === opid) {
+						resolve();
+						return true;
+					} else return false;
+				});
+			});
+		});
 
-    return promise;
-  }
+		this._idle = promise;
 
-  public async load(loader: string | null, ds: string): Promise<void> {
-    const dataset = {
-      ds,
-      loaders: loader !== null ? new Set([loader]) : new Set<string>(),
-    };
+		return promise;
+	}
 
-    this._datasets.push(dataset);
+	public async load(loader: string | null, ds: string): Promise<void> {
+		const dataset = {
+			ds,
+			loaders: loader !== null ? new Set([loader]) : new Set<string>()
+		};
 
-    return this.exec((proc, opid) => proc.stdin.write(`${opid} load ${ds}\n`));
-  }
+		this._datasets.push(dataset);
 
-  public async render(
-    ds: string,
-    gene: string,
-    groupBy: string,
-    splitBy: string,
-  ): Promise<string> {
-    if (!this._datasets.some((dataset) => dataset.ds === ds))
-      throw new Error(`Attempt to render unloaded dataset ${ds}`);
+		return this.exec((proc, opid) => proc.stdin.write(`${opid} load ${ds}\n`));
+	}
 
-    // TODO: kinda sus lol
-    let id: string;
-    return this.exec((proc, opid) => {
-      proc.stdin.write(`${opid} render ${ds} ${gene} ${groupBy} ${splitBy}\n`);
-      id = opid;
-    }).then(() => id!);
-  }
+	public async render(ds: string, gene: string, groupBy: string, splitBy: string): Promise<string> {
+		if (!this._datasets.some((dataset) => dataset.ds === ds)) throw new Error(`Attempt to render unloaded dataset ${ds}`);
 
-  public async unload(ds: string): Promise<void> {
-    if (!this._datasets.some((dataset) => dataset.ds === ds))
-      throw new Error(`Attempt to unload unloaded dataset ${ds}`);
+		// TODO: kinda sus lol
+		let id: string;
+		return this.exec((proc, opid) => {
+			proc.stdin.write(`${opid} render ${ds} ${gene} ${groupBy} ${splitBy}\n`);
+			id = opid;
+		}).then(() => id!);
+	}
 
-    this._datasets.splice(
-      this._datasets.findIndex((dataset) => dataset.ds === ds),
-      1,
-    );
+	public async unload(ds: string): Promise<void> {
+		if (!this._datasets.some((dataset) => dataset.ds === ds)) throw new Error(`Attempt to unload unloaded dataset ${ds}`);
 
-    return this.exec((proc, opid) =>
-      proc.stdin.write(`${opid} unload ${ds}\n`),
-    );
-  }
+		this._datasets.splice(
+			this._datasets.findIndex((dataset) => dataset.ds === ds),
+			1
+		);
 
-  public kill(): void {
-    this.proc.kill();
-  }
+		return this.exec((proc, opid) => proc.stdin.write(`${opid} unload ${ds}\n`));
+	}
+
+	public kill(): void {
+		this.proc.kill();
+	}
 }
+
