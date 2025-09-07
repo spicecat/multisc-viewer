880423f0031177d49a4e0a2b052f214e98cdd938
Andy Teh <ateh0701@gmail.com>
2025-09-06 17:01:04 -0700

    data readme

--- multisc-daemon/genes.R
+++ multisc-daemon/genes.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 
 library(SeuratObject)
-rds <- readRDS(file = "./data.rds", refhook = NULL)
+rds <- readRDS(file = "data.rds", refhook = NULL)
 genes <- rownames(rds@assays$RNA@counts)
-jsonlite::write_json(genes, "./genes.json")
+jsonlite::write_json(genes, "genes.json")
